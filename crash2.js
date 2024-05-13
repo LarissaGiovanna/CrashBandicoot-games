62523594eb2311d8ed8849f53bdb1bbe55b69bf5
LarissaGiovanna <lgiova544@gmail.com>
2024-05-12 23:12:19 -0300

    commit

---
+++ crash2.js
@@ -0,0 +1,5 @@
+let img = document.getElementById('#gif')
+let game = document.getElementById('#game')
+game.addEventListener('click', ()=>{
+    img.style.display = 'none'
+})
---
+++ script.js
@@ -0,0 +1,20 @@
+let logoImg = document.getElementById('#logo-img');
+logoImg.addEventListener('click', () => {
+    window.location.href = 'index.html'
+})
+
+function loadCrash1() {
+    window.location.href = 'crash1.html'
+}
+function loadCrash2() {
+    window.location.href = 'crash2.html'
+}
+function loadCrash3() {
+    window.location.href = 'crash3.html'
+}
+function loadCtr() {
+    window.location.href = 'ctr.html'
+}
+function loadCrashBash() {
+    window.location.href = 'crashBash.html'
+}
