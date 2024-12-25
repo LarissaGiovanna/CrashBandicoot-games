e9835f1ba11ee6651dd24e9e8fd7f0e95d6c7a70
LarissaGiovanna <lgiova544@gmail.com>
2024-12-24 22:06:09 -0300

    adicionadas as sessões resumo, historia, personagens e curiosidades na pag crash2

--- pages/js/crash2.js
+++ pages/js/crash2.js
@@ -1,5 +1,32 @@
-let img = document.getElementById('#gif')
-let game = document.getElementById('#game')
-game.addEventListener('click', ()=>{
-    img.style.display = 'none'
-})
+let resumo = document.getElementById('resumo');
+let curiosidades = document.getElementById('curiosidades');
+let historia = document.getElementById('historia');
+let personagens = document.getElementById('personagens');
+
+function openResumo() {
+    resumo.style.display = 'block';
+    curiosidades.style.display = 'none';
+    historia.style.display = 'none';
+    personagens.style.display = 'none';
+}
+
+function openCuriosidades() {
+    resumo.style.display = 'none';
+    curiosidades.style.display = 'block';
+    historia.style.display = 'none';
+    personagens.style.display = 'none';
+}
+
+function openHistoria() {
+    resumo.style.display = 'none';
+    curiosidades.style.display = 'none';
+    historia.style.display = 'block';
+    personagens.style.display = 'none';
+}
+
+function openPersonagens() {
+    resumo.style.display = 'none';
+    curiosidades.style.display = 'none';
+    historia.style.display = 'none';
+    personagens.style.display = 'block';
+}
